57069fbee62f32d7a87645a9b965afa66b0b399f
TowKnee0 <Tonyyqhe@gmail.com>
2021-03-26 11:46:05 -0400

    Fixed text overflow

--- lib/pages/authenticate/login_page.dart
+++ lib/pages/authenticate/login_page.dart
@@ -29,95 +29,105 @@ class _LoginFormState extends State<LoginForm> {
         backgroundColor: Colors.white,
         body: Container(
             padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-            child: Form(
-                key: _formKey,
-                child: Column(children: <Widget>[
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                      validator: (val) => val.isEmpty ? 'Enter email' : null,
-                      decoration: const InputDecoration(
-                        prefixIcon: Icon(Icons.person),
-                        labelText: 'Email *',
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                          borderSide: BorderSide(color: Colors.blue, width: 2),
+            child: SingleChildScrollView(
+              child: Form(
+                  key: _formKey,
+                  child: Column(children: <Widget>[
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                        validator: (val) => val.isEmpty ? 'Enter email' : null,
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.person),
+                          labelText: 'Email *',
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12.0)),
+                            borderSide:
+                                BorderSide(color: Colors.blue, width: 2),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                            borderSide: BorderSide(color: Colors.blue),
+                          ),
                         ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                          borderSide: BorderSide(color: Colors.blue),
-                        ),
-                      ),
-                      onChanged: (val) {
-                        setState(() => email = val);
-                      }),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                      validator: (val) =>
-                          val.length <= 5 ? 'Enter password (>5 chars)' : null,
-                      decoration: const InputDecoration(
-                        prefixIcon: Icon(Icons.lock),
-                        labelText: 'Password *',
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                          borderSide: BorderSide(color: Colors.blue, width: 2),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                          borderSide: BorderSide(color: Colors.blue),
-                        ),
-                      ),
-                      obscureText: true,
-                      onChanged: (val) {
-                        setState(() => password = val);
-                      }),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  SizedBox(
-                    width: 175,
-                    child: RaisedButton(
-                        color: Colors.blue,
-                        child: Text(
-                          'Sign In',
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
-                        ),
-                        onPressed: () async {
-                          if (_formKey.currentState.validate()) {
-                            dynamic result = await _auth
-                                .signInWithEmailAndPassword(email, password);
-                            if (result == null) {
-                              setState(() => error = 'Sign in failed');
-                            } else {
-                              Navigator.pushNamed(context, 'page_wrapper');
-                            }
-                          }
+                        onChanged: (val) {
+                          setState(() => email = val);
                         }),
-                  ),
-                  SizedBox(
-                    width: 175,
-                    child: RaisedButton(
-                        child: Text(
-                          'Anon',
-                          style: TextStyle(fontWeight: FontWeight.bold),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                        validator: (val) => val.length <= 5
+                            ? 'Enter password (>5 chars)'
+                            : null,
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.lock),
+                          labelText: 'Password *',
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12.0)),
+                            borderSide:
+                                BorderSide(color: Colors.blue, width: 2),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                            borderSide: BorderSide(color: Colors.blue),
+                          ),
                         ),
-                        onPressed: () async {
-                          if (_formKey.currentState.validate()) {
-                            dynamic result = await _auth
-                                .signInWithEmailAndPassword(email, password);
-                            if (result == null) {
-                              setState(() => error = 'Sign in failed');
-                            } else {
-                              Navigator.pushNamed(context, 'page_wrapper');
-                            }
-                          }
+                        obscureText: true,
+                        onChanged: (val) {
+                          setState(() => password = val);
                         }),
-                  ),
-                  SizedBox(height: 12.0),
-                  Text(
-                    error,
-                    style: TextStyle(color: Colors.black, fontSize: 14.0),
-                  ),
-                ]))));
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    SizedBox(
+                      width: 175,
+                      child: RaisedButton(
+                          color: Colors.blue,
+                          child: Text(
+                            'Sign In',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          onPressed: () async {
+                            if (_formKey.currentState.validate()) {
+                              dynamic result = await _auth
+                                  .signInWithEmailAndPassword(email, password);
+                              if (result == null) {
+                                setState(() => error = 'Sign in failed');
+                              } else {
+                                Navigator.pushNamed(context, 'page_wrapper');
+                              }
+                            }
+                          }),
+                    ),
+                    SizedBox(
+                      width: 175,
+                      child: RaisedButton(
+                          child: Text(
+                            'Anon',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          onPressed: () async {
+                            if (_formKey.currentState.validate()) {
+                              dynamic result = await _auth
+                                  .signInWithEmailAndPassword(email, password);
+                              if (result == null) {
+                                setState(() => error = 'Sign in failed');
+                              } else {
+                                Navigator.pushNamed(context, 'page_wrapper');
+                              }
+                            }
+                          }),
+                    ),
+                    SizedBox(height: 12.0),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.black, fontSize: 14.0),
+                    ),
+                  ])),
+            )));
   }
 }
--- lib/pages/authenticate/signup_page.dart
+++ lib/pages/authenticate/signup_page.dart
@@ -47,149 +47,166 @@ class _SignupFormState extends State<SignupForm> {
         backgroundColor: Colors.white,
         body: Container(
             padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-            child: Form(
-                key: _formKey,
-                child: Column(children: <Widget>[
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                      validator: (val) =>
-                          val.isEmpty ? 'Enter first name' : null,
-                      decoration: const InputDecoration(
-                        // icon: Icon(Icons.person),
-                        prefixIcon: Icon(Icons.person),
-                        labelText: 'First Name *',
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                          borderSide: BorderSide(color: Colors.blue, width: 2),
+            child: SingleChildScrollView(
+              child: Form(
+                  key: _formKey,
+                  child: Column(children: <Widget>[
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                        validator: (val) =>
+                            val.isEmpty ? 'Enter first name' : null,
+                        decoration: const InputDecoration(
+                          // icon: Icon(Icons.person),
+                          prefixIcon: Icon(Icons.person),
+                          labelText: 'First Name *',
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12.0)),
+                            borderSide:
+                                BorderSide(color: Colors.blue, width: 2),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                            borderSide: BorderSide(color: Colors.blue),
+                          ),
                         ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                          borderSide: BorderSide(color: Colors.blue),
+                        onChanged: (val) {
+                          setState(() => first = val);
+                        }),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                        validator: (val) =>
+                            val.isEmpty ? 'Enter last name' : null,
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.person),
+                          labelText: 'Last Name *',
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12.0)),
+                            borderSide:
+                                BorderSide(color: Colors.blue, width: 2),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                            borderSide: BorderSide(color: Colors.blue),
+                          ),
                         ),
-                      ),
-                      onChanged: (val) {
-                        setState(() => first = val);
-                      }),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                      validator: (val) =>
-                          val.isEmpty ? 'Enter last name' : null,
-                      decoration: const InputDecoration(
-                        prefixIcon: Icon(Icons.person),
-                        labelText: 'Last Name *',
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                          borderSide: BorderSide(color: Colors.blue, width: 2),
+                        onChanged: (val) {
+                          setState(() => last = val);
+                        }),
+                    SizedBox(height: 20.0),
+                    DropdownButtonFormField<String>(
+                      decoration: InputDecoration(),
+                      hint: Text('Are you a Client or Lawyer?'),
+                      validator: (val) => val.isEmpty ? 'What are you?' : null,
+                      items: <String>['Client', 'Lawyer'].map((String value) {
+                        return new DropdownMenuItem<String>(
+                          value: value,
+                          child: new Text(value),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() => type = value);
+                        isClient = value;
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                        validator: (val) => val.isEmpty ? 'Enter email' : null,
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.mail),
+                          labelText: 'Email *',
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12.0)),
+                            borderSide:
+                                BorderSide(color: Colors.blue, width: 2),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                            borderSide: BorderSide(color: Colors.blue),
+                          ),
                         ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                          borderSide: BorderSide(color: Colors.blue),
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        }),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                        validator: (val) => val.length <= 5
+                            ? 'Enter password (>5 chars)'
+                            : null,
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.lock),
+                          labelText: 'Password *',
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12.0)),
+                            borderSide:
+                                BorderSide(color: Colors.blue, width: 2),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                            borderSide: BorderSide(color: Colors.blue),
+                          ),
                         ),
-                      ),
-                      onChanged: (val) {
-                        setState(() => last = val);
-                      }),
-                  SizedBox(height: 20.0),
-                  DropdownButtonFormField<String>(
-                    decoration: InputDecoration(),
-                    hint: Text('Are you a Client or Lawyer?'),
-                    validator: (val) => val.isEmpty ? 'What are you?' : null,
-                    items: <String>['Client', 'Lawyer'].map((String value) {
-                      return new DropdownMenuItem<String>(
-                        value: value,
-                        child: new Text(value),
-                      );
-                    }).toList(),
-                    onChanged: (value) {
-                      setState(() => type = value);
-                      isClient = value;
-                    },
-                  ),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                      validator: (val) => val.isEmpty ? 'Enter email' : null,
-                      decoration: const InputDecoration(
-                        prefixIcon: Icon(Icons.mail),
-                        labelText: 'Email *',
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                          borderSide: BorderSide(color: Colors.blue, width: 2),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                          borderSide: BorderSide(color: Colors.blue),
-                        ),
-                      ),
-                      onChanged: (val) {
-                        setState(() => email = val);
-                      }),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                      validator: (val) =>
-                          val.length <= 5 ? 'Enter password (>5 chars)' : null,
-                      decoration: const InputDecoration(
-                        prefixIcon: Icon(Icons.lock),
-                        labelText: 'Password *',
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                          borderSide: BorderSide(color: Colors.blue, width: 2),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                          borderSide: BorderSide(color: Colors.blue),
-                        ),
-                      ),
-                      obscureText: true,
-                      onChanged: (val) {
-                        setState(() => password = val);
-                      }),
-                  SizedBox(height: 20.0),
-                  SizedBox(
-                    width: 125,
-                    child: RaisedButton(
-                        color: Colors.blue,
-                        child: Text(
-                          'Sign up',
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
-                        ),
-                        onPressed: () async {
-                          if (_formKey.currentState.validate()) {
-                            dynamic result = await _auth
-                                .registerWithEmailAndPassword(email, password);
-                            Map<String, String> userInfoMap = {
-                              "email": email,
-                              "name": first + " " + last,
-                              "type": type
-                            };
+                        obscureText: true,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        }),
+                    SizedBox(height: 20.0),
+                    SizedBox(
+                      width: 125,
+                      child: RaisedButton(
+                          color: Colors.blue,
+                          child: Text(
+                            'Sign up',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          onPressed: () async {
+                            if (_formKey.currentState.validate()) {
+                              dynamic result =
+                                  await _auth.registerWithEmailAndPassword(
+                                      email, password);
+                              Map<String, String> userInfoMap = {
+                                "email": email,
+                                "name": first + " " + last,
+                                "type": type
+                              };
 
-                            databaseMethods.uploadUserInfo(userInfoMap);
-                            if (result == null) {
-                              setState(() => error = 'Invalid values');
-                            } else {
-                              if (isClient == 'Client') {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => Searching()));
-                              } else if (isClient == 'Lawyer') {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => Expertise()));
+                              databaseMethods.uploadUserInfo(userInfoMap);
+                              if (result == null) {
+                                setState(() => error = 'Invalid values');
+                              } else {
+                                if (isClient == 'Client') {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Searching()));
+                                } else if (isClient == 'Lawyer') {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Expertise()));
+                                }
                               }
                             }
-                          }
-                        }),
-                  ),
-                  SizedBox(height: 12.0),
-                  Text(
-                    error,
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ]))));
+                          }),
+                    ),
+                    SizedBox(height: 12.0),
+                    Text(
+                      error,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ])),
+            )));
   }
 }
